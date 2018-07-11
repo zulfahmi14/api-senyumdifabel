3f1fdd0b9efe0752f1c42c16b2d3748487ca4c52
Nuh Satria <nuhsatria@gmail.com>
2018-07-11 21:31:00 +0700

    api delete bookmark

--- src/main/java/com/example/senyumdifabel/bookmark/BookmarkRepository.java
+++ src/main/java/com/example/senyumdifabel/bookmark/BookmarkRepository.java
@@ -8,4 +8,7 @@ import java.util.List;
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
     @Query("select u from Bookmark u where u.user_id = ?1")
     List<Bookmark> findMark(Long user_id);
+    
+    @Query("select u from Bookmark u where u.user_id = ?1 and u.job_id = ?2")
+    Bookmark findDelete(Long user_id, Long job_id);
 }
